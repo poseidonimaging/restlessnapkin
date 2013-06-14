bd7ddbca3106b657855f132b6a7f7f01c2dfe2d6
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-06-14 14:16:30 -0500

    Added postgres within :production to app.rb

--- app.rb
+++ app.rb
@@ -2,8 +2,26 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'open-uri'
+require 'uri'
 
-set :database, "sqlite3:///orders.db"
+configure :development do
+  set :database, "sqlite3:///orders.db"
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 
 class Order < ActiveRecord::Base
 end
