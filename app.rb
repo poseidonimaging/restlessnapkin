1b77ef55e664097d18bfaec8b25c5225308aa1c2
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-06-14 11:57:29 -0500

    Database and flows all working properly

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ helpers do
   end
 
   def lastname
-    session[:identity] ? session[:identity] : 'Hello Stranger'
+    session[:lastname] ? session[:lastname] : 'Hello Stranger'
   end
   
   def venue
@@ -35,64 +35,91 @@ helpers do
   end
 end
 
+#Shows all orders by all users
 get '/' do
   @orders = Order.order("created_at DESC")
   erb :"orders/index"
 end
 
+post '/' do
+  @orders = Order.order("created_at DESC")
+  erb :"orders/index"
+end
+
+#Looks at the venue and the phone number(splat), then shows form for checkin
 get '/:venue/checkin/*' do 
   session[:venue] = params['venue']
   session[:phone] = params[:splat]
   erb :"orders/checkin"
 end
 
-post '/checkin/attempt' do
-  session[:firstname] = params['firstname']
-  session[:identity] = params['lastname']
-  session[:table] = params['table']
-  
-  redirect '/orders/drinks'
+# After checkin, shows form for drink order. Upon reordering, keeps session via lastname
+post '/orders/drinks' do
+  if session[:lastname]
+    @order = Order.new
+    erb :"orders/drinks"
+  else
+    @order = Order.new
+    session[:lastname] = params['lastname']
+    session[:firstname] = params['firstname']
+    session[:table] = params['table']
+    erb :"orders/drinks"
+  end
 end
 
-# Get the New Order form
-get '/orders/drinks' do
-  @order = Order.new
-  erb :"orders/drinks"
-end
-
-# New Order form sends POST request here
+# New order form sends POST request here
 post '/orders' do
   @order = Order.new(params[:order])
+  @order.venue = session[:venue]
+  @order.table = session[:table]
+  @order.firstname = session[:firstname]
+  @order.lastname = session[:lastname]
+  @order.phone = session[:phone]
   if @order.save
-    redirect "order/#{@order.id}"
+    redirect "/confirm"
   else
     erb :"orders/drinks"
   end
 end
 
 # Get individual orders
-get "/orders/:id" do
-  @order = Order.find(params[:id])
-  @lastname = @order.lastname
-  erb :"orders/show"
+#get "/orders/:id" do
+#  @order = Order.find(params[:id])
+#  erb :"orders/show"
+#end
+
+#Get orders by user's table
+get "/orders/:venue" do
+  @order = Order.find(params[:venue])
+  erb :"orders/table"
 end
 
-post '/confirm' do
-  erb :"orders/confirm"
+# Provides next step options to user after order
+get '/confirm' do
+  erb :"/confirm"
 end
 
+# Showing all orders via confirm screen
+post '/orders/index' do
+  @orders = Order.order("created_at DESC")
+  erb :"orders/index"
+end
+
+#User checkout of venue
 get '/checkout' do
-  session.delete(:identity)
+  session.delete(:lastname)
   erb "<div class='alert alert-success'>Your check will arrive shortly</div>"
 end
 
+#User checkout of venue
 post '/checkout' do
-  session.delete(:identity)
+  session.delete(:lastname)
   erb "<div class='alert alert-success'>Your check will arrive shortly</div>"
 end
 
+#Ancillary secure pages from sinatra/bootstrap github repo
 before '/secure/*' do
-  if !session[:identity] then
+  if !session[:lastname] then
     session[:previous_url] = request.path
     @error = 'Sorry guacamole, you need to be logged in to visit ' + request.path
     halt erb(:checkin)
