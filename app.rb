3f8569616d0c97abc9e30aa802c80d0e4c81279c
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-06-18 15:35:43 -0500

    Added limit of 20 to db query to avoid heavy order load times

--- app.rb
+++ app.rb
@@ -85,12 +85,12 @@ end
 
 #Shows all orders by all users
 get '/' do
-  @orders = Order.order("created_at DESC")
+  @orders = Order.order("created_at DESC").limit(20)
   erb :"orders/index", :layout => (request.xhr? ? false : :layout)
 end
 
 post '/' do
-  @orders = Order.order("created_at DESC")
+  @orders = Order.order("created_at DESC").limit(20)
   erb :"orders/index", :layout => (request.xhr? ? false : :layout)
 end
 
@@ -192,13 +192,13 @@ end
 # Get orders by venue
 get "/venue/:venue" do
   @venue = params[:venue]
-  @orders = Order.where(:venue => params[:venue]).order("created_at DESC")
+  @orders = Order.where(:venue => params[:venue]).order("created_at DESC").limit(20)
   erb :"venue/show", :layout => (request.xhr? ? false : :layout)
 end
 
 # Get orders by user(phone)
 get "/user/:phone" do
-  @orders = Order.where(:phone => params[:phone]).order("created_at DESC")
+  @orders = Order.where(:phone => params[:phone]).order("created_at DESC").limit(20)
   erb :"user/show", :layout => (request.xhr? ? false : :layout)
 end
 
@@ -206,7 +206,7 @@ end
 get "/venue/:venue/table/:table" do
   @venue = params[:venue]
   @table = params[:table]
-  @orders = Order.where(:venue => params[:venue],:table => params[:table]).order("created_at DESC")
+  @orders = Order.where(:venue => params[:venue],:table => params[:table]).order("created_at DESC").limit(20)
   erb :"venue/table/show", :layout => (request.xhr? ? false : :layout)
 end
 
