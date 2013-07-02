61fa2ffd59bc81e15896cbdd0933d0e0243ef73e
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-07-02 13:46:48 -0500

    Added pretty date back to app.rb

--- app.rb
+++ app.rb
@@ -77,21 +77,21 @@ helpers do
     session[:table] ? session[:table] : 'No Table'
   end
 
- # def pretty_date(stamp)
- #   now = Time.new
- #   diff = now - stamp
- #   day_diff = ((now - stamp) / 86400).floor
+ def pretty_date(stamp)
+   now = Time.new
+   diff = now - stamp
+   day_diff = ((now - stamp) / 86400).floor
  
-#    day_diff == 0 && (
- #     diff < 60 && "just now" ||
-#      diff < 120 && "1 minute ago" ||
-#      diff < 3600 && (diff / 60).floor.to_s + " minutes ago" ||
-#      diff < 7200 && "1 hour ago" ||
-#      diff < 86400 && (diff/3600).floor.to_s + " hours ago") ||
-    #day_diff == 1 && "Yesterday" ||
-    # day_diff < 7 && day_diff.to_int + " days ago" ||
-    # day_diff < 31 && (day_diff.to_int / 7).ceil + " weeks ago";
-  #end
+   day_diff == 0 && (
+     diff < 60 && "just now" ||
+     diff < 120 && "1 minute ago" ||
+     diff < 3600 && (diff / 60).floor.to_s + " minutes ago" ||
+     diff < 7200 && "1 hour ago" ||
+     diff < 86400 && (diff/3600).floor.to_s + " hours ago") ||
+   day_diff == 1 && "Yesterday" ||
+   day_diff < 7 && day_diff.to_s + " days ago" ||
+   day_diff < 31 && (day_diff.to_s / 7).ceil + " weeks ago";
+  end
 end
 
 #Shows all orders by all users
