ac61e88492fad7b38a6969ee4664fbe1bb2f5e8e
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-09-19 13:48:10 -0500

    Adding initial menu

---
+++ charge.rb
@@ -0,0 +1,12 @@
+# Edit menu items
+get '/:venue/food/menu' do
+  @venue = Venue.find_by_handle(params[:venue])
+  @item = MenuItem.where(:venue_id => @venue.id)
+  erb :chargeform
+end
+
+# Add a menu item
+get '/admin/:venue/food/add' do
+  @venue = Venue.find_by_handle(params[:venue])
+  erb :"/food/add"
+end
