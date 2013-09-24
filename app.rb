2df7b89431ea7923bc8f8b2dd2d08a7ac9fe1a74
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-09-24 13:38:47 -0500

    Working on getting printer to work as well as food menu testing

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ load "./barkeeper.rb"
 load "./venue.rb"
 load "./liquor.rb"
 load "./food.rb"
+load "./charge.rb"
 load "./admin.rb"
 
 configure do
@@ -135,7 +136,26 @@ get '/mprinter/add_device' do
   client = OAuth2::Client.new(MPRINTER_OAUTH_CLIENT, MPRINTER_OAUTH_SECRET, :site => MPRINTER_OAUTH_URL, :token_url => MPRINTER_OAUTH_URL + '/token')
   client.connection.response :logger
   token = client.client_credentials.get_token({ :client_id => MPRINTER_OAUTH_CLIENT, :client_secret => MPRINTER_OAUTH_SECRET })
-  token.post(MPRINTER_OAUTH_URL + '/devices', :serial => 'KMHELM')
+  response = token.post('/api/v1/devices', :body => {:serial => 'KMHELM'})
+  
+  erb "#{response.body}"
+  #puts token.get(MPRINTER_OAUTH_URL + "/devices").response.body.to_s
+  #RestClient.post MPRINTER_OAUTH_URL + '/devices', :serial => 'KMHELM', token.headers
+end
+
+get '/mprinter/device/status' do
+  RestClient.log = logger
+
+  MPRINTER_OAUTH_CLIENT = 'Ehfv3Qk44jJiB8bifM3A'
+  MPRINTER_OAUTH_SECRET = 'g91EciYab2LdB83eKaRm'
+  MPRINTER_OAUTH_URL    = 'http://manage.themprinter.com/api/v1'
+  
+  client = OAuth2::Client.new(MPRINTER_OAUTH_CLIENT, MPRINTER_OAUTH_SECRET, :site => MPRINTER_OAUTH_URL, :token_url => MPRINTER_OAUTH_URL + '/token')
+  client.connection.response :logger
+  token = client.client_credentials.get_token({ :client_id => MPRINTER_OAUTH_CLIENT, :client_secret => MPRINTER_OAUTH_SECRET })
+  response = token.get('/api/v1/devices/52408f0303d816b6b600003d')
+  
+  erb "#{response.body}"
   #puts token.get(MPRINTER_OAUTH_URL + "/devices").response.body.to_s
   #RestClient.post MPRINTER_OAUTH_URL + '/devices', :serial => 'KMHELM', token.headers
 end
--- food.rb
+++ food.rb
@@ -25,3 +25,10 @@ get '/admin/:venue/food/add' do
   @venue = Venue.find_by_handle(params[:venue])
   erb :"/food/add"
 end
+
+# Show menu
+get '/:venue/food/menu' do
+  @venue = Venue.find_by_handle(params[:venue])
+  @item = MenuItem.where(:venue_id => @venue.id)
+  erb :"/food/menu"
+end
