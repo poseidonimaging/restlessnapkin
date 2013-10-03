c924d6d34546a1045e67efac271361aa90b952eb
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-10-03 13:18:16 -0500

    May finally have mprinter add_device and confirm code working

--- app.rb
+++ app.rb
@@ -143,6 +143,23 @@ get '/mprinter/add_device' do
   #RestClient.post MPRINTER_OAUTH_URL + '/devices', :serial => 'KMHELM', token.headers
 end
 
+get '/mprinter/confirm_device' do
+  RestClient.log = logger
+
+  MPRINTER_OAUTH_CLIENT = 'Ehfv3Qk44jJiB8bifM3A'
+  MPRINTER_OAUTH_SECRET = 'g91EciYab2LdB83eKaRm'
+  MPRINTER_OAUTH_URL    = 'http://manage.themprinter.com/api/v1'
+  
+  client = OAuth2::Client.new(MPRINTER_OAUTH_CLIENT, MPRINTER_OAUTH_SECRET, :site => MPRINTER_OAUTH_URL, :token_url => MPRINTER_OAUTH_URL + '/token')
+  client.connection.response :logger
+  token = client.client_credentials.get_token({ :client_id => MPRINTER_OAUTH_CLIENT, :client_secret => MPRINTER_OAUTH_SECRET })
+  response = token.post('/api/v1/devices/confirm/id', :body => {:code => 'DD3VKL'})
+  
+  erb "#{response.body}"
+  #puts token.get(MPRINTER_OAUTH_URL + "/devices").response.body.to_s
+  #RestClient.post MPRINTER_OAUTH_URL + '/devices', :serial => 'KMHELM', token.headers
+end
+
 get '/mprinter/device/status' do
   RestClient.log = logger
 
