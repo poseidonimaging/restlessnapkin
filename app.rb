066b546669dfc428c6c56d0f96d52fe489c25bd7
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-12-07 18:45:50 -0600

    Food Dashboard formatting changes

--- app.rb
+++ app.rb
@@ -154,7 +154,7 @@ get '/mprinter/confirm_device' do
   client = OAuth2::Client.new(MPRINTER_OAUTH_CLIENT, MPRINTER_OAUTH_SECRET, :site => MPRINTER_OAUTH_URL, :token_url => MPRINTER_OAUTH_URL + '/token')
   client.connection.response :logger
   token = client.client_credentials.get_token({ :client_id => MPRINTER_OAUTH_CLIENT, :client_secret => MPRINTER_OAUTH_SECRET })
-  response = token.post('/api/v1/devices/confirm/id', :body => {:code => 'DD3VKL'})
+  response = token.post('/api/v1/devices/confirm/529f7338449aa8a96b00001a', :body => {:code => 'VNWZKL'})
   
   erb "#{response.body}"
   #puts token.get(MPRINTER_OAUTH_URL + "/devices").response.body.to_s
@@ -171,7 +171,24 @@ get '/mprinter/device/status' do
   client = OAuth2::Client.new(MPRINTER_OAUTH_CLIENT, MPRINTER_OAUTH_SECRET, :site => MPRINTER_OAUTH_URL, :token_url => MPRINTER_OAUTH_URL + '/token')
   client.connection.response :logger
   token = client.client_credentials.get_token({ :client_id => MPRINTER_OAUTH_CLIENT, :client_secret => MPRINTER_OAUTH_SECRET })
-  response = token.get('/api/v1/devices/52408f0303d816b6b600003d')
+  response = token.get('/api/v1/devices/529f7338449aa8a96b00001a')
+  
+  erb "#{response.body}"
+  #puts token.get(MPRINTER_OAUTH_URL + "/devices").response.body.to_s
+  #RestClient.post MPRINTER_OAUTH_URL + '/devices', :serial => 'KMHELM', token.headers
+end
+
+get '/mprinter/device/html' do
+  RestClient.log = logger
+
+  MPRINTER_OAUTH_CLIENT = 'Ehfv3Qk44jJiB8bifM3A'
+  MPRINTER_OAUTH_SECRET = 'g91EciYab2LdB83eKaRm'
+  MPRINTER_OAUTH_URL    = 'http://manage.themprinter.com/api/v1'
+  
+  client = OAuth2::Client.new(MPRINTER_OAUTH_CLIENT, MPRINTER_OAUTH_SECRET, :site => MPRINTER_OAUTH_URL, :token_url => MPRINTER_OAUTH_URL + '/token')
+  client.connection.response :logger
+  token = client.client_credentials.get_token({ :client_id => MPRINTER_OAUTH_CLIENT, :client_secret => MPRINTER_OAUTH_SECRET })
+  response = token.post('/api/v1/queue/add/html/529f7338449aa8a96b00001a', :body => {:data => '<h1>Richard Gentry is an Improv GOD</h1>'})
   
   erb "#{response.body}"
   #puts token.get(MPRINTER_OAUTH_URL + "/devices").response.body.to_s
