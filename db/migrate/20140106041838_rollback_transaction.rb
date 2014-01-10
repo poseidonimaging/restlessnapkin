9da0ddd71908b265cb7077dc988c199f9793feed
Chad Sakonchick <chadwicksakonchick@gmail.com>
2014-01-09 20:28:10 -0600

    Delete 20140106041838_rollback_transaction.rb

--- db/migrate/20140106041838_rollback_transaction.rb
+++
@@ -1,7 +0,0 @@
-class RollbackTransaction < ActiveRecord::Migration
-  def rollback
-604:       execute "rollback transaction"
-605:       @transaction_active = false
-606:       true
-607:     end
-end
